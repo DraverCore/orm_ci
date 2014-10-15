65f5f58aade62da0efb7ba0f368dd644e6efaf76
Marcos Sanchez <draver13@hotmail.com>
2014-10-14 23:11:24 -0500

    Creación de .htaccess.php

---
+++ CodeIgniter_2.2.0/.htaccess.php
@@ -0,0 +1,6 @@
+<IfModule mod_rewrite.c>
+    RewriteEngine On
+    RewriteCond %{REQUEST_FILENAME} !-f
+    RewriteCond %{REQUEST_FILENAME} !-d
+    RewriteRule ^(.*)$ /orm_ci/index.php/$1 [L]
+</IfModule>
